1bbbf5006e46a401d49f1c6646d4c507edf429e8
Truong Tang <txt2408@gmail.com>
2023-06-13 14:35:29 +0700

    fix email validation

--- local-addons/morons/models/contributor.py
+++ local-addons/morons/models/contributor.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
+import re
 
 class InternalUser(models.Model):
     """MercTrans Internal Users"""
@@ -8,3 +9,11 @@ class InternalUser(models.Model):
     contributor = fields.Boolean(string='Contributor', default=False)
     currency = fields.Many2one('res.currency', string='Currency')
 
+    @api.constrains('login')
+    def validate_email(self):
+        if self.login:
+            match = re.match(
+                '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$',
+                self.login)
+            if match is None:
+                raise ValidationError('Not a valid email')
